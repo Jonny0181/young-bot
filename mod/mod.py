3cd79aa997a66f2de731ce7f0acb0628d2cdcac4
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2016-12-20 07:21:42 -0600

    Update mod.py

--- mod/mod.py
+++ mod/mod.py
@@ -993,7 +993,7 @@ class Mod:
             description=created_at,
             colour=discord.Colour(value=colour))
         data.add_field(name="Region", value=str(server.region))
-        data.add_field(name="Users", value="<:vpOnline:212789758110334977>{}<:vpAway:212789859071426561>{}<:vpDnD:236744731088912384>{}<:vpOffline:212790005943369728>{}\n**Humans:** {}\n**Bots:** {}\n**Total {}**".format(a, b, c, d, total, bots, total2))
+        data.add_field(name="Users", value="<:vpOnline:212789758110334977>{}<:vpAway:212789859071426561>{}<:vpDnD:236744731088912384>{}<:vpOffline:212790005943369728>{}\n**Humans:** {}\n**Bots:** {}\n**Total:** {}".format(a, b, c, d, total, bots, total2))
         data.add_field(name="Text Channels", value=text_channels)
         data.add_field(name="Voice Channels", value=voice_channels)
         data.add_field(name="Verification Level", value=vl)
