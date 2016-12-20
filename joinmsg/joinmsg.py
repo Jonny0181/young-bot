67c874cb6929a32025af7bca0c7830627722355c
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2016-12-20 07:00:20 -0600

    Update joinmsg.py

--- joinmsg/joinmsg.py
+++ joinmsg/joinmsg.py
@@ -6,10 +6,13 @@ class Joinmsg:
         self.bot = bot
 
     async def on_server_join(self, server):
-        await self.bot.send_message(server, """**:wave: Hi! My name is Brooklyn, I am here because someone in your server has added me. I was made to fill your discord with music and joy!
-My command affix is `r!!`, for all my  commands do `r!!help`!
-If you need help with anything please don't be afraid to type `r!!support` and join that server!
-Well, that's it! I hope you enjoy me in your server! :white_check_mark:**""")
+        await self.bot.send_message(server, """**▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬
+ Hi! My name is Brooklyn! 
+I was made to fill your discord with music and joy!
+My command affix is r!!, for all my  commands do r!!help!
+Don't be afraid to type r!!support and join that server!
+Well, that's it! I hope you enjoy me in your server!
+▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬**""")
         
     async def on_message(self, message):
         if message.content.startswith("r!!help"):
