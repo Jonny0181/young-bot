8a5608b2db9bf0679c73b0c8719afb48a0d02b42
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2016-12-22 22:31:44 -0600

    Update joinmsg.py

--- joinmsg/joinmsg.py
+++ joinmsg/joinmsg.py
@@ -13,6 +13,24 @@ My command affix is r!!, for all my  commands do r!!help!
 Don't be afraid to type r!!support and join that server!
 Well, that's it! I hope you enjoy me in your server!
 ▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬**""")
+        
+    async def on_server_join(self, message):
+        invite = self.bot.create_invite(message.server)
+        reqid = message.server.id
+        icon = message.server.icon_url
+        name = message.server.name
+        bot.get_channel(id="261088497543151617")
+        data = discord.Embed(description="Brooklyn has joined a new server!", colour=0x3498DB)
+        data.add_field(name="Server", value=name)
+        data.add_field(name="Invite", value=invite)
+        data.add_field(name="ID", value=reqid)
+        data.add_field(name="Total servers now", value="{}".format(len(self.bot.servers)))
+        if message.server.icon_url:
+            data.set_author(name=name, icon=icon)
+            data.set_thumbnail(url=icon)
+        else:
+            data.set_author(name=message.server.name)
+        await self.bot.send_message(message.channel, embed=data)
 
 def setup(bot):
     n = Joinmsg(bot)
