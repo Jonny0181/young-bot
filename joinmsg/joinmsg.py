df16389023c8a0cca3d8917c411224660de39f44
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2016-12-29 07:56:07 -0600

    Update joinmsg.py

--- joinmsg/joinmsg.py
+++ joinmsg/joinmsg.py
@@ -6,31 +6,12 @@ class Joinmsg:
         self.bot = bot
 
     async def on_server_join(self, server):
-        await self.bot.send_message(server, """**▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬
+        await self.bot.send_message(server, """**
  Hi! My name is Brooklyn! 
 I was made to fill your discord with music and joy!
 My command affix is r!!, for all my  commands do r!!help!
 Don't be afraid to type r!!support and join that server!
-Well, that's it! I hope you enjoy me in your server!
-▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬**""")
-        
-    async def on_server_join(self, message):
-        invite = self.bot.create_invite(message.server)
-        reqid = message.server.id
-        icon = message.server.icon_url
-        name = message.server.name
-        bot.get_channel(id="261088497543151617")
-        data = discord.Embed(description="Brooklyn has joined a new server!", colour=0x3498DB)
-        data.add_field(name="Server", value=name)
-        data.add_field(name="Invite", value=invite)
-        data.add_field(name="ID", value=reqid)
-        data.add_field(name="Total servers now", value="{}".format(len(self.bot.servers)))
-        if message.server.icon_url:
-            data.set_author(name=name, icon=icon)
-            data.set_thumbnail(url=icon)
-        else:
-            data.set_author(name=message.server.name)
-        await self.bot.send_message(message.channel, embed=data)
+Well, that's it! I hope you enjoy me in your server!**""")
 
 def setup(bot):
     n = Joinmsg(bot)
