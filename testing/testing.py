bd8fae4a5565a97b39c54a63382d00b27260c94d
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2016-12-29 07:22:49 -0600

    Add files via upload

---
+++ testing/testing.py
@@ -0,0 +1,19 @@
+import discord
+from cogs.utils.chat_formatting import pagify
+
+class Testing:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def listservers(self):
+        """List servers the bot is in."""
+        e = [e.name for e in bot.servers]
+        msg = "\n".format(("\n".join(e)))
+        something = pagify(msg, ["\n"])
+        for page in something:
+            bot.say(page)
+
+def setup(bot):
+    n = Testing(bot)
+    bot.add_cog(n)
