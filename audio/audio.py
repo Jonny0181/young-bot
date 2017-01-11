5278cda8fe6e19b764be82f9a99a43cc5ea5d83e
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-01-10 19:53:42 -0600

    Update audio.py

--- audio/audio.py
+++ audio/audio.py
@@ -1798,7 +1798,7 @@ class Audio:
             msg.add_field(name="Title", value="{}".format(song.title))
             msg.add_field(name="Author", value="{}".format(song.creator))
             msg.add_field(name="Uploader", value="{}".format(song.uploader))
-            msg.add_field(name="Views", value="{}".format(song.views_count))
+            msg.add_field(name="Views", value="{}".format(song.view_count))
             msg.add_field(name="Duration", value="{}".format(dur))
             msg.set_footer(text="Link: <{}>".format(song.webpage_url))
             await self.bot.say(embed=qmsg)
